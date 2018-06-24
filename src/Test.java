2ffbcc9250f39356bd8711cbc0724e7cce2cc988
venkatasankarapu <sankarapu@ad.tri.sbc.com>
2018-06-24 18:39:47 -0500

    addede Hello

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,12 @@
 public class Test {
 	
 	public static void main (String[] args) {
-		System.out.println("This is first programme");
+		
+		for ( int i=0;i<10;i++) {
+			
+		
+		System.out.println("Hello world"+i);
+		}
 	}
 
 }
