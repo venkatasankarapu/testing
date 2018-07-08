2f24f224812c2be791274e5268cc8cf00ef2bc9a
venkatasankarapu <sankarapu@ad.tri.sbc.com>
2018-07-08 13:04:50 -0500

    :q

--- src/Test.java
+++ src/Test.java
@@ -3,10 +3,10 @@ public class Test {
 	
 	public static void main (String[] args) {
 		
-		for ( int i=0;i<10;i++) {
-			
+		for ( int i=0;i<10;i++) {			
 		
 		System.out.println("Number: "+i);
+		System.out.println("Second Number"+i/2);
 		}
 	}
 
